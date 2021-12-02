fd1814632b39fa1b289b1fbe85c41e5b607cf137
nsonb <son.bui@giosg.com>
2021-12-03 00:47:38 +0200

    2nd day done

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        Day1 day1 = new Day1();
-        day1.solvefirst();
-        day1.solvesecond();
+        // Day1 day1 = new Day1();
+        // day1.solvefirst();
+        // day1.solvesecond();
+
+        Day2 day2 = new Day2();
+        // day2.solveFirst();
+        day2.solvesecond();
     }
 }
