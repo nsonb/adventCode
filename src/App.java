5ab829eaa3a42e82e1f0a40d0bedfa148729aa2b
nsonb <son.bui@giosg.com>
2021-12-04 15:46:29 +0200

    day 3 done

--- src/App.java
+++ src/App.java
@@ -1,11 +1,16 @@
+// const k = document.querySelector('pre').innerText.split('\n')
 public class App {
     public static void main(String[] args) {
         // Day1 day1 = new Day1();
         // day1.solvefirst();
         // day1.solvesecond();
 
-        Day2 day2 = new Day2();
+        // Day2 day2 = new Day2();
         // day2.solveFirst();
-        day2.solvesecond();
+        // day2.solvesecond();
+
+        Day3 day3 = new Day3();
+        // day3.solvefirst();
+        day3.solvesecond();
     }
 }
--- src/Day2.java
+++ src/Day2.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Day2 {
   String[] puzzleInput = {
     "forward 8",
